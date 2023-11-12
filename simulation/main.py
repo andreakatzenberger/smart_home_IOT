75ea63dc6335655ecc5090d1c98c9570bfaff9c2
andreakatzenberger <andrea.katzenberger01@gmail.com>
2023-11-12 13:08:18 +0100

    Created Room PIR simulators

--- simulation/main.py
+++ simulation/main.py
@@ -4,25 +4,28 @@ from components.dht import run_dht
 from components.DS1 import run_ds1
 from components.DL import run_dl
 from components.DPIR1 import run_dpir1
+from components.RPIR1 import run_rpir1
+from components.RPIR2 import run_rpir2
 import time
 
 try:
     import RPi.GPIO as GPIO
+
     GPIO.setmode(GPIO.BCM)
 except:
     pass
 
 if __name__ == "__main__":
     print('Starting app: smart_home_iot')
-    settings = load_settings() 
+    settings = load_settings()
     threads = []
-    stop_event = threading.Event() 
+    stop_event = threading.Event()
     print_lock = threading.Lock()
     delay = 7
 
     try:
         dht1_settings = settings['DHT1']
-        run_dht(dht1_settings, threads, stop_event, delay, print_lock)  
+        run_dht(dht1_settings, threads, stop_event, delay, print_lock)
 
         ds1_settings = settings['DS1']
         db_settings = settings['DB']
@@ -30,8 +33,13 @@ if __name__ == "__main__":
 
         dl_settings = settings['DL']
         run_dl(dl_settings, threads, stop_event, delay, print_lock)
+
         dpir1_settings = settings['DPIR1']
         run_dpir1(dpir1_settings, threads, stop_event, delay, print_lock)
+        rpir1_settings = settings['RPIR1']
+        run_rpir1(rpir1_settings, threads, stop_event, delay, print_lock)
+        rpir2_settings = settings['RPIR2']
+        run_rpir2(rpir2_settings, threads, stop_event, delay, print_lock)
 
         while True:
             time.sleep(3)
@@ -39,6 +47,6 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         print('Stopping app: smart_home_iot')
         for t in threads:
-            stop_event.set()  
+            stop_event.set()
 
-        #GPIO.cleanup()
+            # GPIO.cleanup()
